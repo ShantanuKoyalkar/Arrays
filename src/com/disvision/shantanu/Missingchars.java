ab49d7ecb9e1269b138730451d15cb272f8f5197
shantanukoyalkar <shantanukoyalkar19@gmail.com>
2023-01-08 17:18:39 +0530

    added file for missingchars

---
+++ src/com/disvision/shantanu/Missingchars.java
@@ -0,0 +1,27 @@
+package com.disvision.shantanu;
+
+import java.util.Scanner;
+
+public class Missingchars {
+
+	public static void main(String[] args) {
+		int count=0;
+		try {
+		System.out.println("Enter any string that you wanr to measure chars");
+		Scanner scanner= new Scanner(System.in);
+		String s = scanner.nextLine();
+		for(int i=0;i<s.length();i++) {
+			char ch=s.charAt(i);
+			//count=0;
+			if(ch==65) {
+			count++;	
+			}
+		}System.out.println(count);
+		scanner.close();
+
+	}catch(Exception e) {
+		System.out.println(e);
+	}
+		}
+
+}
